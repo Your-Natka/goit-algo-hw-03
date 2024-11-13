6b227f666c8e9dc8428c92e5cb2bf18db6dce2d3
Your-Natka <140067844+Your-Natka@users.noreply.github.com>
2024-11-13 15:00:41 +0100

    finish

--- exemple_1.py
+++ exemple_1.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-
+start_date = "2020-10-09"
 
 def get_days_from_today(date_str):
     try:
-        target_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-        date_today = datetime.today().date()
-        difference = (date_today - target_date).days
-        return difference
+        target_date = datetime.strptime(date_str, "%Y-%m-%d").date()  # Use date_str instead of start_date
+        print("Target date:", target_date)
+        date_today = datetime.now().date()
+        print("Today's date:", date_today)
+        diff_day = (date_today - target_date)
+        return diff_day.days
     except ValueError:
         return "Неправильний формат: {}".format(date_str)
 
-print(get_days_from_today("неправильний формат"))
-print(get_days_from_today("2020-10-09"))
+# Call the function with the correct argument
+print(get_days_from_today(start_date))
